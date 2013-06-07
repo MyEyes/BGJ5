20494a6694955402ed144031b8f4facf09e863c4
Nils Ole Timm <funkinperson@googlemail.com>
2013-06-07 23:35:23 +0200

    Initial Commit for Bacon Game Jam 5
    TADA!

---
+++ Bacon Game Jam 5/Bacon Game Jam 5/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace Bacon_Game_Jam_5
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
